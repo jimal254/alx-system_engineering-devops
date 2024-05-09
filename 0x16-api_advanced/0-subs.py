ef9e5aae2c4218880f0d6ef440d24181d4afad79
Jimmy <you@example.com>
2024-05-09 19:14:01 +0000

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,15 @@
-import json
+#!/usr/bin/python3
+"subscribers count"
 
-def number_of_subscribers_from_file(subreddit_file):
-    """
-    Return the total number of subscribers on a given subreddit from a local file.
+import requests
 
-    Args:
-        subreddit_file (str): The path to the JSON file containing subreddit information.
 
-    Returns:
-        int: The total number of subscribers, or 0 if the subreddit is invalid or file read fails.
-    """
-    try:
-        # Open the file in read mode
-        with open(subreddit_file, 'r') as file:
-            # Load JSON data from the file
-            data = json.load(file)
-            # Get the total number of subscribers
-            subscribers = data.get("data").get("subscribers")
-            return subscribers
-    except FileNotFoundError:
-        # If the file is not found, return 0
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    data = response.json()
+    if response.status_code != 200:
         return 0
-    except json.JSONDecodeError:
-        # If JSON decoding fails, return 0
-        return 0
+    else:
+        return data['data']['subscribers']
